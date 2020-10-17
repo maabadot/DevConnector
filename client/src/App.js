4ada593a8785ee225ef0034880b5f252b3715fe1
Sergey Bolshakov <spooky5ya@yandex.ru>
2020-10-17 15:45:12 +0300

    React user authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -9,9 +9,19 @@ import Alert from './components/layout/Alert.jsx'
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
+
+if (localStorage.token) {
+    setAuthToken(localStorage.token);
+}
 
 const App = () => {
-  return (
+    useEffect(() => {
+        store.dispatch(loadUser());
+    }, []);
+
+    return (
       <Provider store={store}>
           <Router>
               <Fragment>
@@ -27,7 +37,7 @@ const App = () => {
               </Fragment>
           </Router>
       </Provider>
-  );
+    );
 }
 
 export default App;
