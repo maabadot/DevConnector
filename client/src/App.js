fdf8e4ffa7384d514bd7feb06669e0ef40b228d2
Sergey Bolshakov <spooky5ya@yandex.ru>
2020-10-21 21:34:40 +0300

    Dashboard stuff

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,13 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/routing/PrivateRoute';
 import Alert from './components/layout/Alert.jsx'
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
+import EditProfile from './components/profile-forms/EditProfile';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -32,6 +38,11 @@ const App = () => {
                       <Switch>
                           <Route exact path='/register' component={Register}/>
                           <Route exact path='/login' component={Login}/>
+                          <PrivateRoute exact path='/dashboard' component={Dashboard}/>
+                          <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
+                          <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
+                          <PrivateRoute exact path='/add-experience' component={AddExperience}/>
+                          <PrivateRoute exact path='/add-education' component={AddEducation}/>
                       </Switch>
                   </section>
               </Fragment>
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const config = require('config');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route       GET api/profile/me
 // @desc        GET current user's profile
@@ -137,7 +138,9 @@ router.get('/user/:user_id', async (req, res) => {
 // @access      Private
 router.delete('/', auth, async (req, res) => {
     try {
-        // @todo - remove users posts
+        // Remove user's posts
+        await Post.deleteMany({ user: req.user.id });
+
         // Remove profile
         await Profile.findOneAndRemove({user: req.user.id});
         // Remove user
